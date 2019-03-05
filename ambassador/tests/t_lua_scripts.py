892adad0dae39f9c53b764b6542e95169bfd1682
Flynn <flynn@datawire.io>
2019-03-05 11:03:35 -0500

    Fix import.

--- ambassador/tests/t_lua_scripts.py
+++ ambassador/tests/t_lua_scripts.py
@@ -1,6 +1,6 @@
-from ambassador.tests.kat.abstract_tests import AmbassadorTest, ServiceType, HTTP
-from kat.kat.harness import Query
+from kat.harness import Query
 
+from abstract_tests import AmbassadorTest, ServiceType, HTTP
 
 class LuaTest(AmbassadorTest):
     target: ServiceType
